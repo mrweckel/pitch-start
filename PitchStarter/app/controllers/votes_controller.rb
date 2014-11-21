212db44ad94db57c224e51c2e41c459b5da8a29a
edwin kim <emoney86@gmail.com>
2014-11-21 15:26:48 -0500

    add vote count to index

--- PitchStarter/app/controllers/votes_controller.rb
+++ PitchStarter/app/controllers/votes_controller.rb
@@ -1,6 +1,8 @@
 class VotesController < ApplicationController
+  
   def create
     Vote.create(user_id: 1, pitch_id: params[:pitch_id] )
     redirect_to pitch_path(params[:pitch_id])
   end
+  
 end
