fc256bcf4606dae014121097d5ae07d1e82c2a65
edwin kim <emoney86@gmail.com>
2014-11-20 19:43:05 -0500

    add spec models

---
+++ PitchStarter/spec/models/pitch_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe Pitch do
+
+	before :each do
+		@pitch = FactoryGirl.build(:pitch)
+	end
+
+	it "is valid with title and url" do
+	end
+
+	it "is invalid without a title"
+
+end
