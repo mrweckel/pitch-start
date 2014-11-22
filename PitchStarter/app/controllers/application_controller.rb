fd97cbbd684994b4bb931f33f245fe88579d3c65
mrweckel <mrweckel@gmail.com>
2014-11-22 13:33:47 -0500

    add yt_client to application controller

--- PitchStarter/app/controllers/application_controller.rb
+++ PitchStarter/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :yt_client
+
+  def yt_client
+    @yt_client ||= YouTubeIt::Client.new(:username => YouTubeITConfig.username , :password => YouTubeITConfig.password , :dev_key => YouTubeITConfig.dev_key)
+  end
 end
