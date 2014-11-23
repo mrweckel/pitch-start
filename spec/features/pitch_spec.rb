5211d174310fb5c6395b9cdfe0016e42ec1159d3
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-23 16:47:06 -0500

    Fixed feature test

--- spec/features/pitch_spec.rb
+++ spec/features/pitch_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "Pitches" do
 	scenario "create a new pitch" do
 	count = Pitch.count
-	expect{Pitch.create(title: "newest title", url: "www.espn.com")}.to change{Pitch.count}.by(1)
+	expect{Pitch.create(title: "newest title", url: "www.espn.com", user_id:1)}.to change{Pitch.count}.by(1)
 
 	
 	end
