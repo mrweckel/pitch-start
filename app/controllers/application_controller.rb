2337548850e54534811aee4a3ff7a2ad0927a078
edwin kim & Matt <pair+emoney86.mrweckel@devbootcamp.com>
2014-11-22 19:02:04 -0500

    clean up youtube it gem not using anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  helper_method :yt_client
-
-  def yt_client
-    @yt_client ||= YouTubeIt::OAuth2Client.new(
-      client_access_token: "access_token",
-      client_refresh_token: "refresh_token",
-      client_id: "client_id",
-      client_secret: "client_secret",
-      dev_key: "dev_key",
-      expires_at: "expiration time")
-  end
 end
